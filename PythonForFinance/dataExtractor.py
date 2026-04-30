d01040d7a3c94714f2136b7be2b0c9ea03053f74
Shashwat Sarawagi <shashwatsarawagi97@gmail.com>
2026-04-30 18:00:52 +0100

    Fixed data extraction setup

--- PythonForFinance/dataExtractor.py
+++ PythonForFinance/dataExtractor.py
@@ -1,3 +1,4 @@
+import os
 import blpapi
 from xbbg import blp
 import pandas as pd
@@ -10,6 +11,5 @@ start_date = '2023-09-01'
 end_date = '2023-09-20'
 
 hist_tick_data = blp.bdh(tickers=tickers, flds=fields, start_date=start_date, end_date=end_date)
-
 filename = f'tickdata_{start_date}_{end_date}.csv'
-hist_tick_data.to_csv(DATA_DIR + filename)
+hist_tick_data.write_csv(f'{DATA_DIR}/{filename}')
